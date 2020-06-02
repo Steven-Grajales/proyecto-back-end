de17c9479d680bd655c7f8997cf4930e9a1fbb40
Steven <steven.deibi@hotmail.com>
2020-06-02 12:03:10 -0500

    6. Deploy

--- src/main/java/co/udea/registro/api/RegistroApiApplication.java
+++ src/main/java/co/udea/registro/api/RegistroApiApplication.java
@@ -2,11 +2,9 @@ package co.udea.registro.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class RegistroApiApplication {
 
