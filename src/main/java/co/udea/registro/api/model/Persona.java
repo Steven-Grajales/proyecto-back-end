4c0a75a24a3ab5be9503e83f96b3707758b8f760
Steven <steven.deibi@hotmail.com>
2020-06-02 11:44:26 -0500

    5. Deploy

--- src/main/java/co/udea/registro/api/model/Persona.java
+++ src/main/java/co/udea/registro/api/model/Persona.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name="personas")
 public abstract class Persona {
 
     @Id
